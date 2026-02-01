2baada847ff9686224c556623cb82cd5e29e6f7e
Juniven Saavedra <junivensaavedra@gmail.com>
2026-02-01 23:53:24 +0800

    feat: Add assertion to verify successful execution in CancelBookRoomCommand test

--- tests/business_logic/commands/booking/test_cancel_booking_command.py
+++ tests/business_logic/commands/booking/test_cancel_booking_command.py
@@ -510,3 +510,6 @@ class TestCancelBookRoomCommandExecute(unittest.TestCase):
 
                 # Act
                 success, error = command.execute()
+
+                # Assert
+                self.assertTrue(success)
