b8249f59bd14ecb56cdf22bc6f62c92d6f3d48e5
Juniven Saavedra <junivensaavedra@gmail.com>
2026-02-19 22:22:46 +0800

    feat: Add test for executing search at latest business hour in TestSearchRoomCommandEdgeCases

--- tests/business_logic/commands/booking/test_search_rooms_command.py
+++ tests/business_logic/commands/booking/test_search_rooms_command.py
@@ -453,3 +453,8 @@ class TestSearchRoomCommandEdgeCases(unittest.TestCase):
         mock_db.search_room.assert_called_with(
             "Tennis Court", date(2026, 3, 15), time(6, 0)
         )
+
+    @patch("business_logic.commands.booking.search_rooms_command.db")
+    @patch("business_logic.commands.booking.search_rooms_command.BookingInputService")
+    def test_execute_with_boundary_time_late_evening(self, mock_input_service, mock_db):
+        """Test search at latest business hour (10:00 PM)."""
