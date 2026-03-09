63103939715cb3de9c68b88a1aa584d7ad733bca
Juniven Saavedra <junivensaavedra@gmail.com>
2026-03-09 23:38:57 +0800

    feat: Add execution test for ListMembersCommand with no parameters

--- tests/business_logic/commands/member/test_list_members_command.py
+++ tests/business_logic/commands/member/test_list_members_command.py
@@ -268,3 +268,5 @@ class TestListMembersCommandExecute(unittest.TestCase):
 
         mock_db.show_members.return_value = []
         mock_format_table.return_value = ""
+
+        result = ListMembersCommand().execute()
